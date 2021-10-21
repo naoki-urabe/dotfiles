2a470cbeb06840ecfb2e4f474c908976fa7beb1e
naoki-urabe <na0k1ura6e@gmail.com>
2021-10-21 21:49:35 +0900

    1passwordインストール

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+source .env
 sudo apt update
 sudo apt -y install neovim
 sudo apt -y install tmux
 sudo apt -y install fish
+curl https://cache.agilebits.com/dist/1P/op/pkg/v1.12.2/op_linux_amd64_v1.12.2.zip -o op_linux_amd64_v1.12.2.zip 
+sudo apt -y install unzip
+unzip -f op_linux_amd64_v1.12.2.zip
+rm *.zip
+./op signin $ONE_PASSWORD_DOMAIN $ONE_PASSWORD_MAIL_ADDRESS $ONE_PASSWORD_SECRET_KEY
+
 echo "exec fish" >> ~/.bashrc
