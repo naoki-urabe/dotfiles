da2783b1e1a59a569b78d47f744378687c2e9c20
naoki-urabe <na0k1ura6e@gmail.com>
2022-04-24 21:58:47 +0900

    remove needless installation

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,25 +2,12 @@
 source .env
 sudo apt update
 sudo apt-get update
-sudo apt -y install neovim tmux zsh build-essential unzip curl
+sudo apt -y install neovim tmux zsh build-essential unzip
 curl https://cache.agilebits.com/dist/1P/op/pkg/v1.12.2/op_linux_amd64_v1.12.2.zip -o op_linux_amd64_v1.12.2.zip 
 unzip -f op_linux_amd64_v1.12.2.zip
 rm *.zip
 sudo cp ./op /usr/local/bin
 ./op signin $ONE_PASSWORD_DOMAIN $ONE_PASSWORD_MAIL_ADDRESS $ONE_PASSWORD_SECRET_KEY
-
-#install docker
-sudo apt-get -y install \
-  ca-certificates \
-  gnupg \
-  lsb-release \
-  apt-transport-https
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt install -y docker-ce docker-ce-cli containerd.io
-sudo usermod -aG docker $USER
 
 #install kubectl
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
