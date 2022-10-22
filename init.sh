a974ffd2a0ec2d60d008ff6d42c3f89ea12b3db4
nao_e <nao_e@nao-laptop.localdomain>
2022-10-22 12:46:18 +0900

    install hugo,nodejs,ruby asdf plugin

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo apt update
-sudo apt install -y build-essential unzip
+sudo apt install -y build-essential unzip zlib1g-dev libssl-dev
 make install-ansible
--- scripts/install_asdf_plugins.sh
+++ scripts/install_asdf_plugins.sh
@@ -1,21 +1,11 @@
 #!bin/bash
 
-plugin_names=("chezmoi" "awscli" "terraform" "neovim" "krew" "kubectl" "kubectx" "helm" "tmux" "1password-cli" "golang")
-plugin_urls=("https://github.com/joke/asdf-chezmoi.git" \
-            "https://github.com/MetricMike/asdf-awscli.git" \
-            "https://github.com/asdf-community/asdf-hashicorp.git" \
-            "https://github.com/richin13/asdf-neovim.git" \
-            "https://github.com/jimmidyson/asdf-krew.git" \
-            "https://github.com/asdf-community/asdf-kubectl.git" \
-            "https://gitlab.com/wt0f/asdf-kubectx.git" \
-            "https://github.com/Antiarchitect/asdf-helm.git" \
-            "https://github.com/aphecetche/asdf-tmux.git" \
-            "https://github.com/NeoHsu/asdf-1password-cli.git" \
-            "https://github.com/kennyp/asdf-golang.git")
+plugin_names=("chezmoi" "awscli" "terraform" "neovim" "krew" "kubectl" "kubectx" "helm" "tmux" "1password-cli" "golang" "hugo" "nodejs" "ruby")
+
 idx=0
 for i in "${plugin_names[@]}"
 do
     echo plugin:${plugin_names[$idx]} is added
-    asdf plugin add ${plugin_names[$idx]} ${plugin_urls[$idx]}
+    asdf plugin add ${plugin_names[$idx]}
     idx=$((idx))+1
 done
