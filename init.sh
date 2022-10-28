805ec60c3dd0aef559f4df8babf5805860905fc7
nao_e <nao_e@nao-laptop.localdomain>
2022-10-28 21:13:51 +0900

    install tree command

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo apt update
-sudo apt install -y build-essential unzip zlib1g-dev libssl-dev
+sudo apt install -y build-essential unzip zlib1g-dev libssl-dev tree
 make install-ansible
