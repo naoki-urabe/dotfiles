bd5621fa9cdd5a39a73b84a1f86052912827c36b
nao_e <nao_e@nao-laptop>
2023-09-05 12:45:57 +0900

    chezmoi&ansible方式からbash script方式変更

--- dot_config/nvim/plugins/deol.rc.vim
+++
@@ -1,3 +0,0 @@
-nnoremap <silent><C-o> :<C-u>Deol -split=floating -cwd="./"><CR>
-:tnoremap <silent> jj <C-\><C-n> 
-autocmd TermOpen * startinsert
--- scripts/asdf_pre_install_plugins.sh
+++
@@ -1,6 +0,0 @@
-plugin_names=("chezmoi" "1password-cli")
-for plugin in ${plugin_names[@]}
-do
-    asdf install $plugin latest
-    asdf global $plugin latest
-done
